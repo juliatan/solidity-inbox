b9e8c6c7184eab0dcc5576106ed0265416695376
Julia Tan <juliajtan@gmail.com>
2021-11-28 18:12:43 +0000

    Add tests for Inbox contract

--- compile.js
+++ compile.js
@@ -10,5 +10,6 @@ const source = fs.readFileSync(inboxPath, 'utf8')
 
 // 1 is the number of contracts we want to compile
 // From the return object of the compile, we only care about the contracts key
+// Within this object, the 2 keys we're interested in are: bytecode and interface
 // Export this so it can be used by other files
 module.exports = solc.compile(source, 1).contracts[':Inbox']
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,30 +6,45 @@ const Web3 = require('web3') // note that Web3 is capitalized. We're importing a
 // The reason why we might want to have multiple instances of Web3 is to connect to different networks.
 // More likely than not, we will only be connecting to one network.
 const web3 = new Web3(ganache.provider())
+const { interface, bytecode } = require('../compile')
 
 // run test suite: npm run test
-class Car {
-  park() {
-    return 'stopped'
-  }
 
-  drive() {
-    return 'vroom'
-  }
-}
+// Mocha starts -> deploy new contract -> manipulate contract -> make assertion about contract -> loop deploy new contract
+// beforeEach, we should deploy a new contract (need a sender account)
+// ganache automatically creates 10 sender accounts for us, that are unlocked (makes our lives easier)
 
-let car
+let accounts
+let inbox
 
-beforeEach(() => {
-  car = new Car()
+beforeEach(async () => {
+  // Get list of all accounts
+  // Get eth module from web3 (to do with ethereum)
+  // Pretty much all functions in web3 are asynchronous.
+  accounts = await web3.eth.getAccounts()
+
+  // Use one of those accounts to deploy the contract
+  // Contract is capitalized. We're going to create an instance of it.
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] }) // our Inbox contract needs to be initialized with a message
+    .send({ from: accounts[0], gas: '1000000' })
 })
 
-describe('Car', () => {
-  it('can park', () => {
-    assert.equal(car.park(), 'stopped')
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address) // checks that address is a defined / truthy value
   })
 
-  it('can drive', () => {
-    assert.equal(car.drive(), 'vroom')
+  it('creates an instance with an initial message', async () => {
+    const message = await inbox.methods.message().call()
+    assert.equal(message, 'Hi there!')
+  })
+
+  it('can change the message', async () => {
+    // send arguments implies who is paying for this transaction
+    // return value is the transaction hash. If it fails, this will throw an error.
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] })
+    const message = await inbox.methods.message().call()
+    assert.equal(message, 'bye')
   })
 })
