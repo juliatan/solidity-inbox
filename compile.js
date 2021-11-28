9c9ed368cb96065ae0d378c66c512800ea2acf29
Julia Tan <juliajtan@gmail.com>
2021-11-28 17:11:45 +0000

    Add compile code for Inbox contract

--- compile.js
+++ compile.js
@@ -1,1 +1,14 @@
 // compile code will go here
+const path = require('path')
+const fs = require('fs')
+const solc = require('solc')
+
+// use path to ensure it works across windows, unix etc
+// __dirname is the pwd provided by node
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
+const source = fs.readFileSync(inboxPath, 'utf8')
+
+// 1 is the number of contracts we want to compile
+// From the return object of the compile, we only care about the contracts key
+// Export this so it can be used by other files
+module.exports = solc.compile(source, 1).contracts[':Inbox']
